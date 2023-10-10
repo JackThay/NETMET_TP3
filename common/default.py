edd36a12f615c610023d2c96ef39b126bd0f24f8
Jack <56638954+JackThay@users.noreply.github.com>
2023-10-10 10:33:44 +0200

    Add files via upload

---
+++ common/default.py
@@ -0,0 +1,47 @@
+"""main path for output files"""
+from pathlib import Path
+
+# Default path
+DEFAULT_PATH: Path = Path(__file__).resolve().parent
+
+
+##############################################################################################
+# TP1                                                                                        #
+##############################################################################################
+TP1_PATH: Path = DEFAULT_PATH / "../TP1/"
+TP1_DATASET_PATH: Path = TP1_PATH / "datasets"
+
+# dataset
+TP1_PROBES_PATH: Path = TP1_DATASET_PATH / "probes.json"
+TP1_ANCHORS_PATH: Path = TP1_DATASET_PATH / "anchors.json"
+
+# results
+TP1_RESULTS_PATH: Path = TP1_PATH / "results"
+
+
+##############################################################################################
+# TP2                                                                                        #
+##############################################################################################
+TP2_PATH: Path = DEFAULT_PATH / "../TP2/"
+TP2_DATASET_PATH: Path = TP2_PATH / "datasets"
+
+# dataset
+TP2_VPS_DATASET: Path = TP2_DATASET_PATH / "ua_vps.json"
+TP2_TARGETS_DATASET: Path = TP2_DATASET_PATH / "ru_targets.json"
+
+# datasets correction
+TP2_VPS_DATASET_CORRECTION: Path = TP2_DATASET_PATH / "ua_vps_correction.json"
+TP2_TARGETS_DATASET_CORRECTION: Path = TP2_DATASET_PATH / "ru_targets_correction.json"
+
+# results
+TP2_RESULTS_PATH: Path = TP2_PATH / "results"
+
+
+##############################################################################################
+# TP3                                                                                        #
+##############################################################################################
+TP3_PATH: Path = DEFAULT_PATH / "../TP3/"
+TP3_DATASET_PATH: Path = TP3_PATH / "datasets"
+
+# results
+TP3_RESULTS_PATH: Path = TP3_PATH / "results"
---
+++ common/file_utils.py
@@ -0,0 +1,64 @@
+"""Functions to load and save data into a json format.
+All the paths are given in default.py file.
+"""
+import pickle
+
+import ujson as json
+from pathlib import Path
+
+
+def load_json(file_path: Path):
+    """load data at input path"""
+    with open(file_path) as f:
+        return json.load(f)
+
+
+def dump_json(data: list, file_path: Path):
+    """dump data to output file"""
+    # check that dirs exits
+    if not file_path.parent.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def insert_json(data: dict, file_path: Path) -> None:
+    """insert into json struct"""
+    existing_data = []
+
+    if file_path.exists():
+        with file_path.open("r") as f:
+            try:
+                existing_data = json.load(f)
+            except json.JSONDecodeError:
+                pass
+
+    with file_path.open("w") as f:
+        if type(data) == list:
+            existing_data.extend(data)
+        if type(data) == dict:
+            existing_data.append(data)
+        json.dump(existing_data, f, indent=4)
+
+
+def load_pickle(file_path: Path) -> None:
+    """load data from pickle file"""
+    with open(file_path, "rb") as f:
+        return pickle.load(f)
+
+
+def dump_pickle(data: list, file_path: Path):
+    """dum data with pickle to output file path"""
+    # check that dirs exits
+    if not file_path.parent.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(file_path, "wb") as f:
+        pickle.dump(data, f)
+
+
+if __name__ == "__main__":
+    t = [1, 2, 3, 4]
+    insert_json(t, Path("test.json"))
+    insert_json(t, Path("test.json"))
